2f4a011b06b22fbc559fea37833910230afa3e43
Анастасия <nastenka_gubarenko@mail.ru>
2020-12-15 01:37:04 +0800

    FormFindNumber

--- src/js/toggleGuests.js
+++ src/js/toggleGuests.js
@@ -2,6 +2,7 @@ window.onload = function () {
     let t = document.getElementById("guests");
     let idElem = $("#guests");
     let flag = true;
+    let elements = $(".toggle-menu__input");
     idElem.click(function (e) {
         let x = t.clientWidth - e.clientX;
         if (x < 44) {
@@ -21,6 +22,7 @@ window.onload = function () {
         let x = t.clientWidth - e.clientX;
         if (x < 44) {
             t.style.cursor = 'pointer';
+            t.style.color = "rgba(31, 32, 65, 0.75)";
         } else {
             t.style.cursor = 'default';
         }
@@ -32,36 +34,39 @@ window.onload = function () {
         let adultsUser = $(e.currentTarget.previousElementSibling);
         let buttonSub = e.currentTarget.previousElementSibling.previousElementSibling;
         let val = parseInt(adultsUser.val());
-        if (val === 0) {
-            buttonSub.classList.remove("bg-disabled")
-        }
+
         adultsUser.val(val + 1);
         adultsUser.change();
         checkInput();
+        sumFields();
     })
     let bgSub = $(".bg-minus")
     bgSub.click(function (e) {
         let adultsUser = $(e.currentTarget.nextElementSibling);
         let buttonAdd = e.currentTarget.nextElementSibling.nextElementSibling;
         let val = parseInt(adultsUser.val());
-        if (val === 0) {
+        if (val == 0) {
             return;
         }
         val--;
         adultsUser.val(val);
         adultsUser.change();
         checkInput();
+        sumFields();
         if (val === 0) {
             e.currentTarget.classList.add("bg-disabled")
         }
     })
+
     function checkInput() {
-        let elements = $(".toggle-menu__input");
         let flag = false;
         for (let e of elements) {
-            if (+e.value != 0) {
+            let buttonSub=e.previousElementSibling;
+            if (+e.value == 0) {
+                buttonSub.classList.add("bg-disabled")
+            } else {
                 flag = true;
-                break;
+                buttonSub.classList.remove("bg-disabled")
             }
         }
         if (flag) {
@@ -70,4 +75,39 @@ window.onload = function () {
             buttonClear.addClass("button-clear-hidden");
         }
     }
+
+    /**
+     * суммируем поля
+     */
+    function sumFields() {
+        let sum = 0;
+        for (let e of elements) {
+            sum += +e.value;
+        }
+        let t = sum + '';
+        t = +t.charAt(t.length - 1);
+        if (sum == 0) {
+            sum = ""
+        } else if (sum >= 5 && sum < 20) {
+            sum += ' гостей'
+        } else if (t == 1) {
+            sum += ' гость'
+        } else if (t < 5) {
+            sum += ' гостя'
+        } else {
+            sum += ' гостей'
+        }
+        $("#guests").val(sum);
+    }
+
+    $("#butClear").click(function(){
+        elements.val(0);
+        checkInput();
+        sumFields();
+    })
+
+    $("#butApple").click(function(){
+        $("#toggleMenu").hide();
+        $("#guests").removeClass("toggle-input-open");
+    })
 }
