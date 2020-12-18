f8090b94cb5b5a1e0f20929a608af1d5868d833a
Анастасия <nastenka_gubarenko@mail.ru>
2020-12-18 23:09:56 +0800

    datepicker

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -1,30 +1,60 @@
-let arrival = $("#arrival");
-let departure = $("#departure");
-let datepicker = $("#datepicker");
+function installRangeDatepicker(fieldFrom, fieldTo) {
 
-datepicker.datepicker({
-    range: true,
-    onSelect: function (e) {
-        let dates = e.split(',');
-        arrival.val(dates[0]);
-        departure.val(dates[1]);
+    let arrival = $(fieldFrom);
+    let departure = $(fieldTo);
+    let datepicker = document.createElement("input");
+    datepicker.style.visibility = 'hidden';
+    datepicker.style.position = 'absolute';
+    datepicker.style.margin = '0';
+    datepicker.style.width = '0';
+    arrival[0].parentNode.insertBefore(datepicker, arrival[0]);
+
+    function setPosDatepicker() {
+        let pos = arrival.position();
+        $(datepicker).css({
+            position: "absolute",
+            marginLeft: 0, marginTop: 0,
+            top: pos.top, left: pos.left
+        })
     }
-})
 
-function setPosDatepicker() {
-    let pos=arrival.position();
-    datepicker.css({
-        position: "absolute",
-        marginLeft: 0, marginTop: 0,
-        top: pos.top, left: pos.left
+    departure.click(function () {
+        setPosDatepicker();
+        $(datepicker).focus();
     })
+    arrival.click(function () {
+        setPosDatepicker();
+        $(datepicker).focus();
+    })
+
+    $(datepicker).datepicker({
+        range: true,
+        onSelect: function (e) {
+            let dates = e.split(',');
+            arrival.val(dates[0]);
+            departure.val(dates[1]);
+            // if(dates.length===2){
+            //     datepicker.datepicker().data().datepicker.hide();
+            // }
+        },
+        onRenderCell(date, cellType) {
+            return {
+                html: '<div><div>' + date.getDate() + '</div></div>',/// Кастомный контент ячейки
+            }
+        },
+        clearButton: true,
+    })
+
+    let htmlCustomBtn = document.createElement("button");
+    htmlCustomBtn.classList.add('datepicker--button');
+    htmlCustomBtn.innerText = "Применить"
+    htmlCustomBtn.onclick = function () {
+        $(datepicker).data().datepicker.hide();
+    };
+    $(datepicker).data().datepicker.$datepicker[0]
+        .getElementsByClassName( 'datepicker--buttons')[0]
+        .appendChild(htmlCustomBtn);
+
 }
 
-departure.click(function () {
-    setPosDatepicker();
-    datepicker.focus()
-})
-arrival.click(function () {
-    setPosDatepicker();
-    datepicker.focus()
-})
+installRangeDatepicker('#arrival', '#departure');
